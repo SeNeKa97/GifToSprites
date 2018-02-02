c7c4897e95af56ae788886e022314b01bc848d1b
Богдан Шевчук < >
2018-02-02 10:40:18 +0200

    Добавьте файлы проекта.

---
+++ gifToSprites/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace gifToSprites
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.sliceButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(13, 3);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 1;
+            this.openFileButton.Text = "Открыть";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // sliceButton
+            // 
+            this.sliceButton.Enabled = false;
+            this.sliceButton.Location = new System.Drawing.Point(297, 3);
+            this.sliceButton.Name = "sliceButton";
+            this.sliceButton.Size = new System.Drawing.Size(75, 23);
+            this.sliceButton.TabIndex = 2;
+            this.sliceButton.Text = "Нарезать";
+            this.sliceButton.UseVisualStyleBackColor = true;
+            this.sliceButton.Click += new System.EventHandler(this.sliceButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(359, 172);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(359, 172);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(94, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 59);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Варианты нарезки";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(84, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Спрайтлист";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 36);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(128, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Отдельные спрайты";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 257);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.sliceButton);
+            this.Controls.Add(this.openFileButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GifToFrames";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button sliceButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ gifToSprites/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace gifToSprites
+{
+    public partial class Form1 : Form
+    {
+        Image gifImg;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog1.FileName = null;
+            openFileDialog1.DefaultExt = ".gif";
+            openFileDialog1.Filter = "Файл анимации |*.gif";
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            gifImg = Image.FromFile(openFileDialog1.FileName);
+            pictureBox1.Image = gifImg;
+            sliceButton.Enabled = true;
+        }
+
+
+
+
+        //Метод используется для нарезания анимации на отдельные файлы изображений, каждый 
+        //из которых содержит в себе отдельный кадр анимации
+        //все полученные изображения помещаются в новую папку, имеющую имя, идентичное имени .gif-файла, 
+        //для чего вырезается расширение из имени файла
+        private void gifToDifferentFrames(Image gif) 
+        {
+            
+            FrameDimension dimension = new FrameDimension(gif.FrameDimensionsList[0]);
+            int frameCount = gif.GetFrameCount(dimension);
+
+            string directoryPath = cropExtension(openFileDialog1.FileName, ".gif"); //получаем путь к создаваемой папке путем вырезания расширения из имени .gif-файла 
+
+            Image[] frames = new Image[frameCount];         //Создаем массив кадров с длинной, равной количеству кадров анимации
+            Graphics g;
+
+            if (!Directory.Exists(directoryPath))           //проверяем, существует ли уже папка с таким названием
+                Directory.CreateDirectory(directoryPath);   //если нет, создаем её
+
+
+            for (int i = 0; i < frameCount; i++) 
+            {
+                frames[i] = new Bitmap(gif.Width, gif.Height, gif.PixelFormat); //Каждый элемент массива кадров инициализируем отдельным bitmap'ом,
+                                                                                //имеющий ширину, высоту и кол-во битов на пиксель как у оригинального .gif
+
+                gif.SelectActiveFrame(dimension, i);                            //извлекаем из .gif текущий кадр
+                g = Graphics.FromImage(frames[i]);                              //создаем новый холст из текущего элемента массива кадров
+                g.DrawImage(gif,0,0);                                           //и переносим кадр анимации на холст
+
+                frames[i].Save(directoryPath + "\\" + "frame_" + (i + 1) + ".png");//сохраняем измененный элемент массива кадров
+            }
+            
+            foreach (var item in frames) 
+                if(item!=null)
+                    item.Dispose();      //освобождаем ресурсы, занимаемые элементами массива кадров
+        }
+
+
+
+
+        //Метод позволяет сохранить кадры анимации в виде её последовательной раскадровки на одном изображении (спрайтлисте)
+        private void gifToSpriteSheet(Image gif)
+        {
+            FrameDimension dimension = new FrameDimension(gif.FrameDimensionsList[0]);
+            int frameCount = gif.GetFrameCount(dimension);
+
+            //обрезаем расширение, чтобы иметь возможность назвать результирующий файл так же, как и .gif-файл,
+            string directoryPath = cropExtension(openFileDialog1.FileName, ".gif");
+
+
+            //создаем новый спрайтлист - bitmap, с высотой .gif-файла и шириной, полученной путем умножения кол-ва кадров анимации
+            //на ширину изображения .gif-файла
+            using (Bitmap spriteSheet = new Bitmap(gif.Width * frameCount, gif.Height, gif.PixelFormat))
+            {
+                //Создаем обьект графики из спрайтлиста для обеспечения возможности рисования на нем
+                using (Graphics g = Graphics.FromImage(spriteSheet))
+                {
+                    
+                    for (int frames = 0; frames < frameCount; frames++)
+                    {
+                        gif.SelectActiveFrame(dimension, frames); //выбираем текущий кадр анимации
+                        g.DrawImage(gif, gif.Width * frames, 0);  //и переносим его на указанную позицию на спрайтлисте
+                    }
+
+                    spriteSheet.Save(directoryPath + "_spritesheet" + ".png"); //сохраняем спрайтлист с названием файла анимации, но с приставкой "_spritesheet"
+                }
+            }
+        }
+
+
+
+        private string cropExtension(string path, string extension) 
+        {
+            StringBuilder sb = new StringBuilder(path);
+            for (int i = 0; i < sb.Length; i++) 
+                if (sb[i] == extension[0]) 
+                    sb.Remove(i, extension.Length);
+
+            return sb.ToString();
+        }
+
+
+
+        private void sliceButton_Click(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+                gifToSpriteSheet(gifImg);
+            else
+                gifToDifferentFrames(gifImg);
+        }
+    }
+}
